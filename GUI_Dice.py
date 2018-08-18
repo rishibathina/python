e82af2bac6976ab44831ddd9c18471f9791e630f
Rishi Bathina <rbathina8@gmail.com>
2018-08-18 09:39:48 -0700

    Cool Graphic User Interface Die

---
+++ GUI_Dice.py
@@ -0,0 +1,77 @@
+from tkinter import *
+import random
+
+class GUIDie(Canvas):
+    '''6-sided Die class for GUI'''
+
+    def __init__(self,master,valueList=[1,2,3,4,5,6],colorList=['black']*6):
+        '''GUIDie(master,[valueList,colorList]) -> GUIDie
+        creates a GUI 6-sided die
+          valueList is the list of values (1,2,3,4,5,6 by default)
+          colorList is the list of colors (all black by default)'''
+        # create a 60x60 white canvas with a 5-pixel grooved border
+        Canvas.__init__(self,master,width=60,height=60,bg='white',\
+                        bd=5,relief=GROOVE)
+        # store the valuelist and colorlist
+        self.valueList = valueList
+        self.colorList = colorList
+        # initialize the top value
+        self.top = 1
+
+    def get_top(self):
+        '''GUIDie.get_top() -> int
+        returns the value on the die'''
+        return self.valueList[self.top-1]
+
+    def roll(self):
+        '''GUIDie.roll()
+        rolls the die'''
+        self.top = random.randrange(1,7)
+        self.draw()
+
+    def draw(self):
+        '''GUIDie.draw()
+        draws the pips on the die'''
+        # clear old pips first
+        self.erase()
+        # location of which pips should be drawn
+        pipList = [[(1,1)],
+                   [(0,0),(2,2)],
+                   [(0,0),(1,1),(2,2)],
+                   [(0,0),(0,2),(2,0),(2,2)],
+                   [(0,0),(0,2),(1,1),(2,0),(2,2)],
+                   [(0,0),(0,2),(1,0),(1,2),(2,0),(2,2)]]
+        for location in pipList[self.top-1]:
+            self.draw_pip(location,self.colorList[self.top-1])
+
+    def draw_pip(self,location,color):
+        '''GUIDie.draw_pip(location,color)
+        draws a pip at (row,col) given by location, with given color'''
+        (centerx,centery) = (15+20*location[1],15+20*location[0])  # center
+        self.create_oval(centerx-5,centery-5,centerx+5,centery+5,fill=color)
+
+    def erase(self):
+        '''GUIDie.erase()
+        erases all the pips'''
+        pipList = self.find_all()
+        for pip in pipList:
+            self.delete(pip)
+
+class DieTest(Frame):
+
+    def __init__(self,master):
+        Frame.__init__(self,master)
+        self.grid()
+        self.die = GUIDie(self,[1,2,3,4,5,-6],['black']*5+['red'])
+        self.die.grid()
+        self.value = Label(self,text='',width=15)
+        self.value.grid()
+        Button(self,text='Roll',command=self.roll).grid()
+
+    def roll(self):
+        self.die.roll()
+        self.value['text'] = 'Value is: '+str(self.die.get_top())
+
+root = Tk()
+dietest = DieTest(root)
+dietest.mainloop()
